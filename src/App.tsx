e0e034d5cba4cdbfbe11915a84f69e7f06d85f12
MyungWanPark <mywanpark@gmail.com>
2021-12-31 13:24:53 +0900

    Connect ts to styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import Circle from "./Circle";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <div>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle text="i'm here" bgColor="tomato" />
-    </div>
+    <Container>
+      <H1>hello</H1>
+    </Container>
   );
 }
 
